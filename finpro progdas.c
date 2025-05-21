6e0fed0b65a9fbace8922bd4f0072fc9e5b93edf
ToriqFathoni <toriqfathoni01@gmail.com>
2025-05-21 20:04:48 +0700

    kerangka program

---
+++ finpro progdas.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char nama[50];
+    int warga_sd;
+    int sd_tersedia;
+    int kapasitas_sd;
+
+    int warga_smp;
+    int smp_tersedia;
+    int kapasitas_smp;
+
+    int warga_sma;
+    int sma_tersedia;
+    int kapasitas_sma;
+} Desa;
+
+void inputDesa (){
+    
+}
+
+void inputData (){
+    
+}
+
+void analisisDesa (){
+    
+}
+
+void tampilkanAnalisis (){
+    
+}
+
+int main (){
+    char nama_kota[50];
+    int jumlah_desa;
+    Desa desa[100]; 
+}
