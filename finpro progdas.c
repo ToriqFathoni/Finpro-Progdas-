6159f2a4ae36491ab90df664d29b79ca234f65a5
ToriqFathoni <toriqfathoni01@gmail.com>
2025-05-22 09:58:35 +0700

    fungsi input desa

--- finpro progdas.c
+++ finpro progdas.c
@@ -16,12 +16,34 @@ typedef struct {
     int kapasitas_sma;
 } Desa;
 
-void inputDesa (){
+void inputDesa(Desa desa[], int i) {
+    printf("Masukkan Nama Desa: ");
+    scanf(" %[^\n]", desa[i].nama);
     
+    printf("Masukkan Jumlah Warga usia SD        : ");
+    scanf("%d", &desa[i].warga_sd);
+    printf("Masukkan Jumlah SD yang tersedia     : ");
+    scanf("%d", &desa[i].sd_tersedia);
+    printf("Masukkan Rata-rata Kapasitas tiap SD : ");
+    scanf("%d", &desa[i].kapasitas_sd);
+
+    printf("Masukkan Jumlah Warga usia SMP       : ");
+    scanf("%d", &desa[i].warga_smp);
+    printf("Masukkan Jumlah SMP yang tersedia    : ");
+    scanf("%d", &desa[i].smp_tersedia);
+    printf("Masukkan Rata-rata Kapasitas tiap SMP: ");
+    scanf("%d", &desa[i].kapasitas_smp);
+
+    printf("Masukkan Jumlah Warga usia SMA       : ");
+    scanf("%d", &desa[i].warga_sma);
+    printf("Masukkan Jumlah SMA yang tersedia    : ");
+    scanf("%d", &desa[i].sma_tersedia);
+    printf("Masukkan Rata-rata Kapasitas tiap SMA: ");
+    scanf("%d", &desa[i].kapasitas_sma);
 }
 
 void inputData (){
-    
+    //buat yang bikin bagian ini tolong nanti parameter buat looping nya pake i ya biar sesuai
 }
 
 void analisisDesa (){
