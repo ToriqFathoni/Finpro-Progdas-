ac7a3c91bb32ed3a14c28ceb725281aa32a412f8
ToriqFathoni <toriqfathoni01@gmail.com>
2025-05-30 15:46:08 +0700

    nambahin input

--- finpro progdas.c
+++ finpro progdas.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 //toriq
 typedef struct {
     char nama[50];
@@ -14,35 +16,117 @@ typedef struct {
     int warga_sma;
     int sma_tersedia;
     int kapasitas_sma;
+
+    int penduduk;
+    int lulusan_d3;
+    int lulusan_s1;
+    int lulusan_s2;
+
+    int sekolah_ada;
+    int total_guru;
+    int guru_pns;
+
+    // Nilai UN SD 
+    int un_sd_bi;  // Bahasa Indonesia
+    int un_sd_bing; // Bahasa Inggris
+    int un_sd_mtk; // Matematika
+
+    // Nilai UN SMP 
+    int un_smp_bi;    // Bahasa Indonesia
+    int un_smp_bing;  // Bahasa Inggris
+    int un_smp_mtk;   // Matematika
+    int un_smp_ipa_ips; // IPA atau IPS
+
+    char kategori[30];
 } Desa;
 
-//toriq & abyan (masukin input dari user)
+// toriq & abyan Fungsi input data desa
 void inputDesa(Desa desa[], int i) {
-    printf("Masukkan Nama Desa: ");
+    printf("\n--- INPUT DATA DESA KE-%d ---\n", i+1);
+    printf("Nama Desa           : ");
     scanf(" %[^\n]", desa[i].nama);
-    
-    printf("Masukkan Jumlah Warga usia SD        : ");
+
+    printf("\nData SD:\n");
+    printf("  Warga usia SD     : ");
     scanf("%d", &desa[i].warga_sd);
-    printf("Masukkan Jumlah SD yang tersedia     : ");
+    printf("  SD tersedia       : ");
     scanf("%d", &desa[i].sd_tersedia);
-    printf("Masukkan Rata-rata Kapasitas tiap SD : ");
+    printf("  Kapasitas/SD      : ");
     scanf("%d", &desa[i].kapasitas_sd);
 
-    printf("Masukkan Jumlah Warga usia SMP       : ");
+    printf("\nData SMP:\n");
+    printf("  Warga usia SMP    : ");
     scanf("%d", &desa[i].warga_smp);
-    printf("Masukkan Jumlah SMP yang tersedia    : ");
+    printf("  SMP tersedia      : ");
     scanf("%d", &desa[i].smp_tersedia);
-    printf("Masukkan Rata-rata Kapasitas tiap SMP: ");
+    printf("  Kapasitas/SMP     : ");
     scanf("%d", &desa[i].kapasitas_smp);
 
-    printf("Masukkan Jumlah Warga usia SMA       : ");
+    printf("\nData SMA:\n");
+    printf("  Warga usia SMA    : ");
     scanf("%d", &desa[i].warga_sma);
-    printf("Masukkan Jumlah SMA yang tersedia    : ");
+    printf("  SMA tersedia      : ");
     scanf("%d", &desa[i].sma_tersedia);
-    printf("Masukkan Rata-rata Kapasitas tiap SMA: ");
+    printf("  Kapasitas/SMA     : ");
     scanf("%d", &desa[i].kapasitas_sma);
+    
+    printf("\n+-- DATA KUALITAS PENGAJAR ---------------------------+\n");
+    printf("|  Jumlah Guru PNS     : ");
+    scanf("%d", &desa[i].guru_pns);
+    printf("|  Jumlah Lulusan D3   : ");
+    scanf("%d", &desa[i].lulusan_d3);
+    printf("|  Jumlah Lulusan S1   : ");
+    scanf("%d", &desa[i].lulusan_s1);
+    printf("|  Jumlah Lulusan S2   : ");
+    scanf("%d", &desa[i].lulusan_s2);
 
+    // Tambahkan total_guru 
+    desa[i].total_guru = desa[i].lulusan_d3 + desa[i].lulusan_s1 + desa[i].lulusan_s2;
+
+    printf("+-----------------------------------------------------+\n");
+
+    // Input nilai UN SD
+    printf("\n+-- NILAI UN SD (0-10) -------------------------------+\n");
+    do {
+        printf("|  Bahasa Indonesia (0-10) : ");
+        scanf("%d", &desa[i].un_sd_bi);
+    } while (desa[i].un_sd_bi < 0 || desa[i].un_sd_bi > 10);
+
+    do {
+        printf("|  Bahasa Inggris (0-10)   : ");
+        scanf("%d", &desa[i].un_sd_bing);
+    } while (desa[i].un_sd_bing < 0 || desa[i].un_sd_bing > 10);
+
+    do {
+        printf("|  Matematika (0-10)       : ");
+        scanf("%d", &desa[i].un_sd_mtk);
+    } while (desa[i].un_sd_mtk < 0 || desa[i].un_sd_mtk > 10);
+    printf("+-----------------------------------------------------+\n");
+
+    // Input nilai UN SMP
+    printf("\n+-- NILAI UN SMP (0-10) -------------------------------+\n");
+    do {
+        printf("|  Bahasa Indonesia (0-10) : ");
+        scanf("%d", &desa[i].un_smp_bi);
+    } while (desa[i].un_smp_bi < 0 || desa[i].un_smp_bi > 10);
+
+    do {
+        printf("|  Bahasa Inggris (0-10)   : ");
+        scanf("%d", &desa[i].un_smp_bing);
+    } while (desa[i].un_smp_bing < 0 || desa[i].un_smp_bing > 10);
+
+    do {
+        printf("|  Matematika (0-10)       : ");
+        scanf("%d", &desa[i].un_smp_mtk);
+    } while (desa[i].un_smp_mtk < 0 || desa[i].un_smp_mtk > 10);
+
+    do {
+        printf("|  IPA / IPS (0-10)        : ");
+        scanf("%d", &desa[i].un_smp_ipa_ips);
+    } while (desa[i].un_smp_ipa_ips < 0 || desa[i].un_smp_ipa_ips > 10);
+    printf("+-----------------------------------------------------+\n");
 }
+
 //fernanda (analisis untuk tiap desa)
 void analisisDesa (int jumlah_desa, Desa desa[]){
     for (int i = 0; i < jumlah_desa; i++){
